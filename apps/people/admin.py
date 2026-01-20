31f07fe0bed64f4db87fe72edf34ff225e4a8775
Hycinth Yimga <yimgah@yahoo.com.com>
2026-01-20 08:29:54 -0500

    Surface school code in preferences and tidy admissions admin import

--- apps/people/admin.py
+++ apps/people/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from unfold.admin import ModelAdmin
 from .models import TeacherProfile, StudentProfile, StudentGuardian
 
